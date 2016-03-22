5c821ab65f18b13b103867ca59b52dfff01899ac
Mike Reed <mike.reed.vallejo@gmail.com>
2016-03-22 13:17:18 -0700

    add _underscore npm

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -25,15 +26,8 @@ app.get('/todos', function(request, response) {
 
 //GET /todos/:id
 app.get('/todos/:id', function(request, response) {
-    var todoID = request.params.id;
-    var matchedTodo;
-    var i = 0;
-    do {
-        i++;
-        if (String(i) === request.params.id) {
-            matchedTodo = todos[i];
-        }
-    } while ( i <= todos.length && !matchedTodo );
+    var todoID = parseInt(request.params.id);
+    var matchedTodo = _.findWhere(todos, {id: todoID});
     
     if (matchedTodo) {
         response.json(matchedTodo);
@@ -44,7 +38,15 @@ app.get('/todos/:id', function(request, response) {
 
 // POST /todos
 app.post('/todos', function(request, response) {
-    var body = request.body;
+    var body = _.pick(request.body, 'description', 'completed');
+    
+    if (!_.isBoolean(body.completed) || 
+        !_.isString(body.description) || 
+        body.description.trim().length === 0) 
+    {
+        return response.status(400).send();
+    }
+    body.description = body.description.trim();
     
     // add id field to data body
     body.id = todoNextId++;
